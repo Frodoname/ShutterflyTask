6fa2f6eb96bfe321b6152c94b3a963a12c29a225
Frodoname <freeway995@gmail.com>
2024-04-15 15:03:54 +0300

    Initial Commit

---
+++ ShutterflyTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShutterflyTask
+//
+//  Created by Fed on 15.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShutterflyTask/ShutterflyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterflyTaskApp.swift
+//  ShutterflyTask
+//
+//  Created by Fed on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShutterflyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
